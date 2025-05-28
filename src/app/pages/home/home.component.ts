d9c702b099655bc64a106373e8c95f07ba6b3211
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-28 18:13:32 -0300

    Instalando json-server para mock e centralizando footer e header em todas as rotas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
-import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-login',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
