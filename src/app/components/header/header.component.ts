e4db363246d24ece762fe2458314a838a3f4e04e
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-28 17:57:40 -0300

    Criando footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private router: Router) { }
   navigateToLogin() {
-    throw new Error('Method not implemented.');
+    this.router.navigate(['/login'])
   }
 
 }
