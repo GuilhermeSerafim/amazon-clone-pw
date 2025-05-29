fc6dc1df0bbbe6bfa927d3dee78b42d52f65de96
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-29 16:04:32 -0300

    Design base pagina summer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SummerComponent } from './pages/summer/summer.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'summer', component: SummerComponent },
 ];
--- src/app/components/box-items/box-items.component.ts
+++ src/app/components/box-items/box-items.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-box-items',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './box-items.component.scss'
 })
 export class BoxItemsComponent {
+  constructor(private router: Router) { }
+  navigateToPageSummer() {
+    this.router.navigate(['/summer']);
+  }
 
 }
