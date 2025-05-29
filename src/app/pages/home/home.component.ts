98f548963cfa6dcc8eb186fbf0f6c98f0e2ae4aa
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-29 13:21:53 -0300

    Ajustes de estilos, removendo footer e header do global

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderSliderComponent } from "../../components/header-slider/header-slider.component";
 import { BoxItemsComponent } from "../../components/box-items/box-items.component";
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderSliderComponent, BoxItemsComponent],
+  imports: [HeaderSliderComponent, BoxItemsComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
