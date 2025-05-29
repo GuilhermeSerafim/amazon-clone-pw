f26ade2c19984b527651c06e5bced26cb5025f48
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-29 13:37:47 -0300

    Layout page login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  user: any;
+  password: any;
 }
