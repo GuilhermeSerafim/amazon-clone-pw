db9739ad52a6fe9713e2d48f145b6a9444c7f3bf
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-30 01:56:09 -0300

    Adicionando mais itens verão

--- src/app/pages/summer/summer.component.ts
+++ src/app/pages/summer/summer.component.ts
@@ -75,6 +75,78 @@ export class SummerComponent {
       deal: true,
       id: '5',
       disabled: false,
+    },
+    {
+      img: '/assets/verao/6.jpg',
+      title: 'Wekapo Beach Blanket',
+      stars: 4.6,
+      reviews: 879,
+      price: 28.99,
+      delivery: 'FREE delivery Jun 6 - 9',
+      id: '6',
+      disabled: false
+    },
+    {
+      img: '/assets/verao/7.jpg',
+      title: 'Retro Sunglasses Pack',
+      stars: 4.4,
+      reviews: 2102,
+      price: 17.49,
+      delivery: 'FREE delivery Wed, Jun 5',
+      colors: ['#a2b86c', '#a05328', '#f3e6d5', '#000'],
+      id: '7',
+      disabled: false
+    },
+    {
+      img: '/assets/verao/8.jpg',
+      title: 'Summer Glass Jar Set',
+      stars: 4.7,
+      reviews: 532,
+      price: 32.95,
+      delivery: 'FREE delivery Jun 7 - 10',
+      id: '8',
+      disabled: false
+    },
+    {
+      img: '/assets/verao/9.jpg',
+      title: 'Bedsure Cooling Blanket',
+      stars: 4.5,
+      reviews: 1598,
+      price: 42.99,
+      delivery: 'FREE delivery Jun 3 - 6',
+      id: '9',
+      disabled: false
+    },
+    {
+      img: '/assets/verao/11.jpg',
+      title: 'Owala FreeSip Bottle',
+      stars: 4.9,
+      reviews: 1934,
+      price: 24.95,
+      delivery: 'FREE delivery Tue, Jun 4',
+      colors: ['#00bcd4', '#f44336', '#ffc107'],
+      id: '11',
+      disabled: false
+    },
+    {
+      img: '/assets/verao/12.jpg',
+      title: 'Crochet Beach Tote Bag',
+      stars: 4.6,
+      reviews: 876,
+      price: 19.99,
+      delivery: 'FREE delivery Fri, Jun 7',
+      id: '12',
+      disabled: false
+    },
+    {
+      img: '/assets/verao/13.jpg',
+      title: 'Portable Neck Fan',
+      stars: 4.4,
+      reviews: 1323,
+      price: 26.49,
+      delivery: 'FREE delivery Jun 5 - 8',
+      id: '13',
+      disabled: false
     }
   ];
   searchText: string = '';
